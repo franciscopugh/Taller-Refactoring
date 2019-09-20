df319b3f08f9b5070f43ee7ab3b9a8eb9906aee9
TheDarkWolf <franciscopugh01@gmail.com>
2019-09-20 17:47:24 -0300

    Third Class

--- print-the-bill.js
+++ print-the-bill.js
@@ -1,32 +1,73 @@
 module.exports = statement = function (invoice, plays) {
-   let totalAmount = 0;
-   let volumeCredits = 0;
-   let result = `Statement for ${invoice.customer}\n`;
-   const format = new Intl.NumberFormat("en-US",
+      const statementData = {};
+      statementData.customer = invoice.customer;
+      statementData.performances = invoice.performances.map(enrichPerfomance);   //Loop sobre todas las perfomances
+      return renderPlainText(statementData, plays);
+
+      function enrichPerfomance(aPerfomance) {
+         const result = Object.assign({}, aPerfomance);     //Autocopia el arreglo
+         result.play = playFor(result);              
+         return result;
+      }
+
+      function playFor(aPerformance) {
+         return plays[aPerformance.playID];
+      }      
+}
+
+function renderPlainText(data) {
+   let result = `Statement for ${data.customer}\n`;
+
+   for (let perf of data.performances) {
+      result += `  ${perf.play.name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+   }
+
+   result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
+   result += `You earned ${totalVolumeCredits()} credits\n`;
+   return result;
+
+   function totalVolumeCredits() {
+      let result = 0;
+
+      for (let perf of data.performances) {
+         result += volumeCreditsFor(perf);
+      }
+      return result;
+   }
+
+   function totalAmount() {
+      let result = 0;
+      
+      for (let perf of data.performances) {
+         result += amountFor(perf);
+      }
+      return result;
+   }
+
+   function usd(aNumber) {
+
+      return new Intl.NumberFormat("en-US",
       {
          style: "currency", currency: "USD",
          minimumFractionDigits: 2
-      }).format;
+      }).format(aNumber);
 
-   for (let perf of invoice.performances) {
-      let thisAmount = amountFor(perf);
-
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-
-      // print line for this order
-      result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-      totalAmount += thisAmount;
    }
-   result += `Amount owed is ${format(totalAmount / 100)}\n`;
-   result += `You earned ${volumeCredits} credits\n`;
-   return result;
+
+   function volumeCreditsFor(aPerfomance) {
+      let result = 0;
+
+        // add volume credits
+      result = Math.max(aPerfomance.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+      if ("comedy" === aPerfomance.play.type) result += Math.floor(aPerfomance.audience / 5);
+      
+      return result;
+   }
 
    function amountFor(aPerformance) {
       let result = 0; 
-      switch (playFor(aPerformance).type) {
+      switch (aPerformance.play.type) {
          case "tragedy":
             result = 40000;
             if (aPerformance.audience > 30) {
@@ -41,12 +82,9 @@ module.exports = statement = function (invoice, plays) {
             result += 300 * aPerformance.audience;
             break;
          default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+            throw new Error(`unknown type: ${aPerformance.play.type}`);
       }
       return result;
    }
 
-   function playFor(aPerformance) {
-      return plays[aPerformance.playID];
-    }
 }
